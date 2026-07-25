b18579344ab6d7860b3bfcaa45db2764aff87af0
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2026-07-26 00:34:00 +0300

    Final push of the night. Custom color preview element

--- js/canvasControls.js
+++ js/canvasControls.js
@@ -2,19 +2,31 @@ import canvas from "./canvas.js";
 
 /** @type {HTMLInputElement} */
 let colorInput;
-
 /** @type {HTMLButtonElement} */
 let clearBtn;
+/** @type {HTMLDivElement} */
+let colorBtn;
+/** @type {HTMLDivElement} */
+let colorPreview;
 
 function init() {
     // @ts-ignore
     colorInput = document.getElementById("colorPicker");
     // @ts-ignore
     clearBtn = document.getElementById("clearCanvasBtn");
+    // @ts-ignore
+    colorBtn = document.getElementById("colorBtn");
+    // @ts-ignore
+    colorPreview = document.getElementById("colorPreview");
 
     colorInput.addEventListener("input", onColorInputChange);
     clearBtn.addEventListener("click", onClearBtnClick);
+    colorBtn.addEventListener("click", onColorBtnClick);
 
+}
+
+function onColorBtnClick() {
+    colorInput.click();
 }
 
 function onClearBtnClick() {
@@ -23,6 +35,7 @@ function onClearBtnClick() {
 
 function onColorInputChange() {
     canvas.setColor(colorInput.value);
+    colorPreview.style.backgroundColor = colorInput.value;
 }
 
 export default { init };
